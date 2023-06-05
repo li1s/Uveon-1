5473695c3b996fe3974e365121bd9ca6232a11c7
li1s <l1is@yahoo.com>
2023-06-05 10:39:31 +0300

    renamed:    TUI install/README -> Bash/TUI install/README 	renamed:    TUI install/checksystem -> Bash/TUI install/checksystem 	renamed:    TUI install/start.sh -> Bash/TUI install/start.sh 	renamed:    TUI install/usershome -> Bash/TUI install/usershome 	modified:   Optimization VM/Opti.txt 	new file:   Python/test.py

---
+++ Python/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, subprocess, string
+program = input("Введите имя программы для проверки: ")
+try:
+    output = subprocess.getoutput ("ps -auxh | grep" + program)
+    proginfo = str.split(output)
+    #print (proginfo)
+    print ("\n\
+    Путь:\t\t", proginfo[5], "\n\
+    Владелец:\t\t\t", proginfo[0], "\n\
+    ID процесса:\t\t", proginfo[1], "\n\
+    ID родительского процесса:\t", proginfo[2], "\n\
+    Время запуска:\t\t", proginfo[0])
+except Exception as error:
+    print("An exception occurred:", error)
+   # print ("При выполнении программы возникла проблема!")
+
+
